bab7112b46ebaa1a135d3ef104fa455c2784618a
Adam M <53438697+akmiyata@users.noreply.github.com>
2022-07-30 06:27:35 -0700

    Create main_traffic_lights.c
    Starter shell/framework

---
+++ main_traffic_lights.c
@@ -0,0 +1,25 @@
+// Steps & Pseudo
+/* 1. Setup hardware- In progress
+*  Needed: 3 switches (order at least two more); 2 each of green, yellow, red LEDS;  
+*  several wires (detail later); six 220 ohm resistors for LEDS; 3 10K ohm resistors for switch inputs
+*  Red wire- +3.3V power
+*  White wire- PB5  (powers green light on right side of breadboard)
+*  Green wire1- PB0 (powers green light on right side of breadboard)
+*  Yellow wire1- PB1 (powers yellow light on right side of breadboard)
+*  
+*  2. Define/understand requirements
+*  3. Code
+*  4. ???
+*  5. Done! */
+
+// 1. Pre-processor directives
+
+
+// 2. Declarations section
+
+
+// 3. Subroutines
+
+int main(void){
+  
+}
