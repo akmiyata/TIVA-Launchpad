fb46659eff5ba97dd5ce75f03594239a1d34cf41
Adam M <53438697+akmiyata@users.noreply.github.com>
2022-08-04 06:13:25 -0700

    Create TM4C123G_AKM.h
    Header for port B and F register pointers

---
+++ TM4C123G_AKM.h
@@ -0,0 +1,22 @@
+// Port B definitions
+#define GPIO_PORTB_DATA_R 		(*((volatile unsigned long *)0x400053FC))
+#define GPIO_PORTB_DIR_R 		  (*((volatile unsigned long *)0x40005400))
+#define GPIO_PORTB_AFSEL_R 		(*((volatile unsigned long *)0x40005420))
+#define GPIO_PORTB_PUR_R 		  (*((volatile unsigned long *)0x40005510))
+#define GPIO_PORTB_DEN_R 		  (*((volatile unsigned long *)0x4000551C))
+#define GPIO_PORTB_LOCK_R 		(*((volatile unsigned long *)0x40005520))
+#define GPIO_PORTB_CR_R 		  (*((volatile unsigned long *)0x40005524))
+#define GPIO_PORTB_AMSEL_R 		(*((volatile unsigned long *)0x40005528))
+#define GPIO_PORTB_PCTL_R 		(*((volatile unsigned long *)0x4000552C))
+#define SYSCTL_RCGC2_R 		    (*((volatile unsigned long *)0x400FE108))
+
+// Port F definitions
+#define GPIO_PORTF_DATA_R 		(*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R 		  (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R 		(*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R 		  (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R 		  (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R 		(*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R 		  (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R 		(*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R 		(*((volatile unsigned long *)0x4002552C))
