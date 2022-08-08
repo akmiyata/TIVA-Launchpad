606aec0284b89ab3a6264ef8dec8f35de5cc4c62
Adam M <53438697+akmiyata@users.noreply.github.com>
2022-08-08 15:03:10 -0700

    Update main_traffic_lights.c

--- main_traffic_lights.c
+++ main_traffic_lights.c
@@ -20,9 +20,9 @@
 *  PB4 (yellow wire1)- powers yellow light for south street
 *  PB5 (white wire1)- powers red light for south street
 *  
-*  PB6 (blue wire)- Input for west street car sensor 
-*  PB7 (blue wire)- Input for south street car sensor
-*  PE2 (blue wire)- Input for crosswalk
+*  PE0 (blue wire)- Input for west street car sensor 
+*  PE1 (blue wire)- Input for south street car sensor
+*  PA7 (blue wire)- Input for crosswalk
 *  2. Define/understand requirements- COMPLETE
 *  3. Code- In progress
 *  4. Test- After coding 
@@ -32,92 +32,52 @@
 #include "TM4C123G_AKM.h" /* Custom header with Port B, E, and F register pointers */
 #include <stdint.h>
 
-/* Additional address pointers*/
-#define PB6                 (*((volatile uint32_t *)0x40005100)) /* Base address = 0x40005000 (port B) + 0x0100 */
-#define PB7                 (*((volatile uint32_t *)0x40005200)) /* Base address = 0x40005000 (port B) + 0x0200 */
-#define PE2	                (*((volatile uint32_t *)0x40024010)) /* Base address = 0x40024000 (port E) + 0x0010 */
-#define SYSCTL_RCC2_R       (*((volatile uint32_t *)0x400FE070)) /* PLL Initialization */
-#define SYSCTL_RCC_R        (*((volatile uint32_t *)0x400FE060)) /* PLL Initialization */
-#define SYSCTL_RIS_R        (*((volatile uint32_t *)0x400FE050)) /* PLL Initialization- PLLRIS */	
-#define NVIC_ST_CTRL_R      (*((volatile uint32_t *)0xE000E010)) /* SYSTICK TIMER */
-#define NVIC_ST_RELOAD_R    (*((volatile uint32_t *)0xE000E014)) /* SYSTICK TIMER */
-#define NVIC_ST_CURRENT_R   (*((volatile uint32_t *)0xE000E018)) /* SYSTICK TIMER */
+#define SENSOR  (*((volatile uint32_t *)0x4002400C))
+#define LIGHT   (*((volatile uint32_t *)0x400050FC))
+#define goW    0
+#define waitW  1
+#define goS    2
+#define waitS  3
 
+/* Linked data structure */
+struct State {
+	uint32_t Out;
+	uint32_t Time;
+	uint32_t Next[4];};
+
+typedef const struct State State_t;
+	
+State_t FSM[4]={
+{0x21,3000,{goW,waitW,goW,waitW}},
+{0x22, 500,{goS,goS,goS,goS}},
+{0x0C,3000,{goS,goS,waitS,waitS}},
+{0x14, 500,{goW,goW,goW,goW}}};
+
+ uint32_t S; /* Index to the current state */
+ uint32_t Input;
 /* Function prototypes */
 void Delay(void);
-void SysTick_Init(void);
-void SysTick_Wait(uint32_t);
-void SysTick_Wait10ms(uint32_t);
-void PLL_Init(void);
-
 
 void Delay(void){
 	int i = 0;
-	for(i = 0;i<500000;i++){
+	for(i = 0;i<1000000;i++){
 	}
 }
-
-void SysTick_Init(void){
-  NVIC_ST_CTRL_R = 0;               /* disable SysTick during setup */
-  NVIC_ST_CTRL_R = 0x00000005;      /* enable SysTick with core clock */
-}
-
-/* The delay parameter is in units of the 80 MHz core clock. (12.5 ns) */
-void SysTick_Wait(uint32_t delay){
-  NVIC_ST_RELOAD_R = delay-1;  /* number of counts to wait */
-  NVIC_ST_CURRENT_R = 0;       /* any value written to CURRENT clears */
-  while((NVIC_ST_CTRL_R&0x00010000)==0){ /* wait for count flag */
-  }
-}
-/* 800000*12.5ns equals 10ms */
-void SysTick_Wait10ms(uint32_t delay){
-  uint32_t i;
-  for(i=0; i<delay; i++){
-    SysTick_Wait(800000);  /* wait 10ms */
-  }
-}
-
-/*  Phase Lock Loop */
-void PLL_Init(void){
-  SYSCTL_RCC2_R |=  0x80000000;                 /* USE RCC2 */
-  SYSCTL_RCC2_R |=  0x00000800;                 /* BYPASS2, PLL bypass */
-  SYSCTL_RCC_R = (SYSCTL_RCC_R &~0x000007C0)    /* clear XTAL field, bits 10-6 */
-                 + 0x00000540;                  /* 10101, configure for 16 MHz crystal */
-  SYSCTL_RCC2_R &= ~0x00000070;                 /* configure for main oscillator source */
-  SYSCTL_RCC2_R &= ~0x00002000;                 /* activate PLL by clearing PWRDN */
-  SYSCTL_RCC2_R |= 0x40000000;                  /* set the desired system divider; use 400 MHz PLL */
-  SYSCTL_RCC2_R = (SYSCTL_RCC2_R &~0x1FC00000)  /* clear system clock divider */
-                  + (4<<22);                    /* configure for 80 MHz clock */
-  while((SYSCTL_RIS_R&0x00000040)==0){}         /* wait for the PLL to lock by polling PLLRIS */
-  SYSCTL_RCC2_R &= ~0x00000800;                 /* enable use of PLL by clearing BYPASS */
-}
-
 
 int main(void){ volatile uint32_t delay;
-	PLL_Init();
-	SysTick_Init();
-  SYSCTL_RCGC2_R |= 0x00000002; /* Enable clock for Port B */
+  SYSCTL_RCGC2_R |= 0x00000013; /* Enable clock for Port A, B, & E */
 	delay = SYSCTL_RCGC2_R; /* delay */
-	GPIO_PORTB_DIR_R |= 0x3F; /* PB7 & 6 input, PB5-0 output */
+	GPIO_PORTA_DIR_R &= ~0x20; /* PA5 input */
+	GPIO_PORTA_DEN_R |= 0x20; /* PA5 digital enable */
+	GPIO_PORTB_DIR_R |= 0x3F; /* PB5-0 output */
 	GPIO_PORTB_DEN_R |= 0x3F; /* Enable digital I/O on PB5 through PB0 */
-	GPIO_PORTE_DIR_R &= 0x03; /* PE0 & PE1 are inputs */
+	GPIO_PORTE_DIR_R &= ~0x03; /* PE0 & PE1 are inputs */
 	GPIO_PORTE_DEN_R |= 0x03; /* PE0 & PE1 enable digital I/O */
+	S = goW;
 	while(1){
-		/* There are three states: 1. Car senses at west light, 2. Car sensed at south light, 3. Crosswalk request, and 4. No input (no cars sensed, no crosswalk request). */
-		if(PB7 == 1) {
-			
+		LIGHT = FSM[S].Out; /* Set lights */
+		Delay();
+		Input = SENSOR; /* Read input */
+		S = FSM[S].Next[Input];
 		}
-		else if(PB6 == 1){
-			
-		}
-		else if(PE2 == 1){/* Crosswalk button pressed, both sets of lights need to transition to Red*/
-			
-		}
-		GPIO_PORTB_DATA_R = 0x0C; /* Initially, west traffic lights are red, south are green, and onboard LED is Red */
-		SysTick_Wait10ms(500);
-		GPIO_PORTB_DATA_R = 0x14; /* When car is sensed (button pushed), transition to west light red, south light yellow */
-		SysTick_Wait10ms(500);
-		GPIO_PORTB_DATA_R = 0x21; /* When car is sensed (button pushed), to finish transition, west light is green, south light red */
-		SysTick_Wait10ms(500);
 	}
-}
